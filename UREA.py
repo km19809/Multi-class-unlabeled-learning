46ece4a21739f1a8dbfecfa50cb8feb2a6162b67
Amedeo Racanati <moamedterzo@gmail.com>
2021-04-08 23:29:31 +0200

    All hyps for urea and area

--- UREA.py
+++ UREA.py
@@ -25,7 +25,7 @@ class UREA(bc.BaseClassifier):
 
     def get_grid_hyperparameters(self):
         return {
-            'Learning_rate': np.logspace(-4, -1, 4),
+            'Learning_rate': np.logspace(-5, -1, 5),
             'Weight_decay': np.logspace(-5, -1, 5),
         }
 
